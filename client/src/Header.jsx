8b80199d893c22e911bf379dc6133e0ee1db2858
Sahil9309 <sahiltalwekar123@gmail.com>
2025-02-24 17:47:50 +0530

    defined Schema for Places

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 -rotate-90">
               <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
             </svg>
-            <span className='font-bold text-xl'>TickEase</span>
+            <span className='font-bold text-xl'>NestQuest</span>
           </Link>
           <div className='flex gap-2 border border-gray-300 rounded-full px-4 py-2 shadow-md shadow-gray-300'>
             <div>Anywhere</div>
@@ -18,7 +18,7 @@ export default function Header() {
             <div>Any time</div>
             <div className='border-l border-gray-300'></div>
             <div>Add people</div>
-            <button className="bg-[#F76E11] text-white rounded-full p-1"> 
+            <button className="bg-[#1E3A8A] text-white rounded-full p-1"> 
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
                 <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
               </svg>
