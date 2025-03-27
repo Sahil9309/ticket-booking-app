5c4a44399968d2f3efadfac88d6cdfa069e4e666
Sahil9309 <sahiltalwekar123@gmail.com>
2025-03-27 13:58:00 +0530

    staged changes

--- api/index.js
+++ api/index.js
@@ -4,11 +4,14 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('./models/User.js');
+const Place = require('./models/Place.js');
 const cookieParser = require('cookie-parser');
-const imageDownloader = require('images-downloader');
+const imageDownloader = require('image-downloader');
+const multer = require('multer');
+const fs = require('fs');
+
 
 require('dotenv').config();
-
 const app = express();
 
 // Add this line to define bcrypt salt rounds
@@ -16,26 +19,28 @@ const bcryptSalt = bcrypt.genSaltSync(10);
 const jwtSecret = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30';
 
 app.use(express.json());
-app.use(cookieParser()) ;
-
+app.use(cookieParser());
+app.use('/uploads', express.static(__dirname + '/../uploads'));
 app.use(cors({
     credentials: true,
     origin: 'http://localhost:5173',
 }));
 
-console.log(process.env.MONGO_URI);
-// Connect once at server startup
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log('MongoDB connection error:', err));
+const mongoURI = process.env.MONGO_URI;
+if (!mongoURI) {
+    throw new Error('MONGO_URI environment variable is not defined');
+}
 
+mongoose.connect(mongoURI)
+    .then(() => console.log('MongoDB connected successfully'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
-app.get('/api/test', (req,res) => {
+app.get('/api/test', (req, res) => {
     res.json('test ok');
-  });
+});
 
-app.post('/api/register', async (req,res) => {
-  const {name,email,password} = req.body;
+app.post('/api/register', async (req, res) => {
+  const { name, email, password } = req.body;
 
   try {
     const existingUser = await User.findOne({ email });
@@ -46,7 +51,7 @@ app.post('/api/register', async (req,res) => {
     const userDoc = await User.create({
       name,
       email,
-      password:bcrypt.hashSync(password, bcryptSalt),
+      password: bcrypt.hashSync(password, bcryptSalt),
     });
     res.json(userDoc);
   } catch (e) {
@@ -54,17 +59,16 @@ app.post('/api/register', async (req,res) => {
   }
 });
 
-
-app.post('/api/login', async (req,res) => {
-  const {email,password} = req.body;
-  const userDoc = await User.findOne({email});
+app.post('/api/login', async (req, res) => {
+  const { email, password } = req.body;
+  const userDoc = await User.findOne({ email });
   if (userDoc) {
     const passOk = bcrypt.compareSync(password, userDoc.password);
     if (passOk) {
       jwt.sign({
-        email:userDoc.email,
-        id:userDoc._id
-      }, jwtSecret, {}, (err,token) => {
+        email: userDoc.email,
+        id: userDoc._id
+      }, jwtSecret, {}, (err, token) => {
         if (err) throw err;
         res.cookie('token', token).json(userDoc);
       });
@@ -76,25 +80,109 @@ app.post('/api/login', async (req,res) => {
   }
 });
 
-
-app.get('/api/profile', (req,res) => {
-  const {token} = req.cookies;
+app.get('/api/profile', (req, res) => {
+  const { token } = req.cookies;
   if (token) {
     jwt.verify(token, jwtSecret, {}, async (err, userData) => {
       if (err) throw err;
-      const {name,email,_id} = await User.findById(userData.id);
-      res.json({name,email,_id});
+      const { name, email, _id } = await User.findById(userData.id);
+      res.json({ name, email, _id });
     });
   } else {
     res.json(null);
   }
 });
 
-app.post('/api/logout', (req,res) => {
+app.post('/api/logout', (req, res) => {
   res.cookie('token', '').json(true);
 });
 
+app.post('/api/upload-by-link', async (req, res) => {
+  try {
+    const { link } = req.body;
+    if (!link) {
+      return res.status(400).json({ error: 'No link provided' });
+    }
+    const newName = 'photo' + Date.now() + '.jpg';
+    await imageDownloader.image({
+      url: link,
+      dest: __dirname + '/../uploads/' + newName,
+    });
+    res.json(newName);
+  } catch (error) {
+    console.error('Upload error:', error);
+    res.status(500).json({ error: 'Failed to download image' });
+  }
+});
 
+const photosMiddleware = multer({ dest: __dirname + '/../uploads/' });
+app.post('/api/upload', photosMiddleware.array('photos', 100), async (req,res) => {
+  const uploadedFiles = [];
+  for(let i = 0; i < req.files.length; i++) {
+    const {path, originalname} = req.files[i];
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    const newPath = path + '.' + ext;
+    fs.renameSync(path, newPath);
+    uploadedFiles.push(newPath.replace(__dirname + '/../uploads/', ''));
+  }
+  res.json(uploadedFiles);
+});
+
+app.post('/api/places', (req,res) => {
+  const {token} = req.cookies;
+  const {
+    title,address,addedPhotos,description,
+    perks,extraInfo,checkIn,checkOut,maxPeople,price
+  } = req.body;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    if (err) throw err;
+    const placeDoc = await Place.create({
+      owner:userData.id,
+      title,address,photos:addedPhotos,description,
+      perks,extraInfo,checkIn,checkOut,maxPeople,price
+    });
+    res.json(placeDoc);
+  });
+});
+
+app.get('/api/user-places', (req,res) => {
+  const {token} = req.cookies;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    const {id} = userData;
+    res.json( await Place.find({owner:id}) );
+  });
+});
+
+app.get('/api/places/:id', async (req,res) => {
+  const {id} = req.params;
+  res.json(await Place.findById(id));
+});
+
+app.put('/api/places', async (req,res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const {token} = req.cookies;
+  const {
+    id, title,address,addedPhotos,description,
+    perks,extraInfo,checkIn,checkOut,maxPeople,price,
+  } = req.body;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    if (err) throw err;
+    const placeDoc = await Place.findById(id);
+    if (userData.id === placeDoc.owner.toString()) {
+      placeDoc.set({
+        title,address,photos:addedPhotos,description,
+        perks,extraInfo,checkIn,checkOut,maxPeople,price,
+      });
+      await placeDoc.save();
+      res.json('ok');
+    }
+  });
+});
+
+app.get('/api/places/', async (req,res) => {
+  res.json(await Place.find());
+})
 
 app.listen(8000);
-    // console.log('Server running on port 8000');
+
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
-import React from 'react'; // Added import for React
-import './App.css'
+import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import IndexPage from './pages/IndexPage.jsx'
 import LoginPage from './pages/LoginPage.jsx'
@@ -7,8 +6,10 @@ import Layout from './Layout'
 import RegisterPage from './pages/RegisterPage.jsx'
 import axios from 'axios'
 import { UserContextProvider } from './UserContext.jsx'
-import AccountPage from './pages/AccountPage.jsx'
-import PlacesPage from './pages/PlacesPage.jsx' // Added import for PlacesPage
+import ProfilePage from './pages/ProfilePage.jsx'
+import PlacesPage from './pages/PlacesPage.jsx'
+import PlacesFormPage from './pages/PlacesFormPage.jsx'
+import PlacePage from './pages/PlacePage.jsx'
 
 axios.defaults.baseURL = 'http://localhost:8000'; 
 axios.defaults.withCredentials = true;
@@ -18,12 +19,14 @@ function App() {
     <UserContextProvider>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<IndexPage/>}/>
-          <Route path="/login" element={<LoginPage />}/>
-          <Route path="/register" element={<RegisterPage />}/>
-          <Route path='/account/:subpage?' element={<AccountPage/>}></Route>
-          <Route path='/account/:subpage/:action' element={<AccountPage/>}></Route>
-          <Route path='/places' element={<PlacesPage/>}/> {/* Added route for PlacesPage */}
+          <Route index element={<IndexPage />} />
+          <Route path="/login" element={<LoginPage />} /> {/* Corrected path */}
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/account" element={<ProfilePage />} />
+          <Route path="/account/places" element={<PlacesPage />} />
+          <Route path="/account/places/new" element={<PlacesFormPage />} />
+          <Route path="/account/places/:id" element={<PlacesFormPage />} />
+          <Route path='/place/:id' element={<PlacePage/>}/>
         </Route>
       </Routes>
     </UserContextProvider>
