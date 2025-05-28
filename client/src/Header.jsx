b41fb55fa275ad8d024a57026289aad338389f68
Sahil9309 <sahiltalwekar123@gmail.com>
2025-05-28 12:04:37 +0530

    removed unnessecery hardcoded components

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -12,19 +12,19 @@ export default function Header() {
             </svg>
             <span className='font-bold text-xl'>NestQuest</span>
           </Link>
-          <div className='flex gap-2 border border-gray-300 rounded-full px-4 py-2 shadow-md shadow-gray-300'>
+          {/* <div className='flex gap-2 border border-gray-300 rounded-full px-4 py-2 shadow-md shadow-gray-300'>
             <div>Anywhere</div>
             <div className='border-l border-gray-300'></div>
             <div>Any time</div>
             <div className='border-l border-gray-300'></div>
             <div>Add people</div>
-            {/* <button className="bg-[#1E3A8A] text-white rounded-full p-1"> 
+            <button className="bg-[#1E3A8A] text-white rounded-full p-1"> 
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
                 <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
               </svg>
   
-            </button> */}
-          </div>
+            </button>
+          </div> */}
           <Link to={user ? '/account' : '/login'} className='flex items-center gap-2 border border-gray-300 rounded-full px-4 py-2'>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
